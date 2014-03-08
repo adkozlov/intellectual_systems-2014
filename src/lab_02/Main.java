54edfe9869ded18228cc8fd846b96e6aec43855f
Andrew Kozlov <adkozlov@gmail.com>
2014-03-09 02:03:04 +0400

    sets printed

--- src/lab_02/Main.java
+++ src/lab_02/Main.java
@@ -7,6 +7,7 @@ import org.antlr.v4.runtime.CommonTokenStream;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author adkozlov
@@ -18,10 +19,18 @@ public class Main {
             List<String> documents = HTMLParser.getParagraphs(Properties.URL, Properties.REGEX);
             ReverseIndex index = new ReverseIndex(LexicalAnalyzer.analyze(Tokenizer.tokenize(documents)), documents.size());
 
-            CharStream stream = new ANTLRFileStream("karenina.in");
+            CharStream stream = new ANTLRFileStream(Properties.INPUT_FILENAME);
             GrammarLexer lexer = new GrammarLexer(stream);
             GrammarParser parser = new GrammarParser(new CommonTokenStream(lexer));
-            parser.file();
+            List<Set<Integer>> list = parser.file(index).result;
+
+            for (Set<Integer> set : list) {
+                for (int i : set) {
+                    System.out.print((i + 1) + " ");
+                }
+
+                System.out.println();
+            }
         } catch (IOException e) {
             System.err.println(e.getLocalizedMessage());
         }
